e9be34007ff311b79f1f41156615553415ff1cbc
Lucas Lopes <medowlucas@gmail.com>
2022-02-13 23:58:37 -0300

    adicionando padrao viewHolder para mapear a interface

--- app/src/main/java/com/example/conversormoedas/MainActivity.java
+++ app/src/main/java/com/example/conversormoedas/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.conversormoedas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder.editValue = findViewById(R.id.edit_value);
+        this.mViewHolder.textDollar = findViewById(R.id.text_dolar);
+        this.mViewHolder.textEuro = findViewById(R.id.text_euro);
+        this.mViewHolder.buttonCalculate = findViewById(R.id.button_calculate);
     }
+
+    private static class ViewHolder{
+        EditText editValue;
+        TextView textDollar;
+        TextView textEuro;
+        Button buttonCalculate;
+    }
+
 }
